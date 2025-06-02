7ad018d4f74464fa16c7733cb57590b9520c9ee0
arafatmansuri <mansuriarafat302@gmail.com>
2025-06-02 15:36:42 +0530

    Add Tags and Content Interfaces

--- BE/src/types.ts
+++ BE/src/types.ts
@@ -22,3 +22,14 @@ interface ILink {
 export interface ILinkDocument extends ILink, Document {
   deocdeLink: () => string;
 }
+
+export interface ITags extends Document {
+  title: string;
+}
+export interface IContent extends Document {
+  link: string;
+  type: "image" | "video" | "article" | "audio";
+  title: string;
+  tags: Schema.Types.ObjectId[];
+  userId: Schema.Types.ObjectId;
+}
