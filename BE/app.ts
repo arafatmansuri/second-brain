93af47a2279f8c1d52b0ec847c64e53a5e2fddcc
arafatmansuri <mansuriarafat302@gmail.com>
2025-06-02 12:58:32 +0530

    Add app.ts and serve.ts

---
+++ BE/app.ts
@@ -0,0 +1,15 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("check", (req, res) => {
+  res.json({ message: "Working" });
+  return;
+});
+
+export default app;
