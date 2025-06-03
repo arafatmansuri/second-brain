b71275e44ebd52dcc0edc65a48112e4fdd3c830a
arafatmansuri <mansuriarafat302@gmail.com>
2025-06-03 19:24:37 +0530

    Add Dynamic Button components with user given variants and sizes

---
+++ FE/src/components/ui/Button.tsx
@@ -0,0 +1,36 @@
+import type { ReactElement } from "react";
+
+export interface ButtonProps {
+  varient: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  text: string;
+  startIcon?: ReactElement; //React element instead any
+  endIcon?: ReactElement;
+  onClick?: () => void;
+}
+const variantStyles = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-300 text-purple-600",
+};
+const sizeStyles = {
+  lg: "py-3 px-6 gap-1",
+  sm: "py-1 px-2",
+  md: "py-2 px-4 gap-2",
+};
+const defaultStyles = "m-3 rounded-md flex";
+function Button(props: ButtonProps) {
+  console.log(props.size);
+  return (
+    <button
+      className={`${defaultStyles} ${variantStyles[props.varient]} 
+      ${sizeStyles[props.size]}
+        `}
+      onClick={props.onClick}
+    >
+      {props.startIcon}
+      <span>{props.text}</span>
+      {props.endIcon}
+    </button>
+  );
+}
+export default Button;
