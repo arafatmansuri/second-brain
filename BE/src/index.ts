0e60f4f52c5a7eff3de3fc43725d2731e6328400
arafatmansuri <mansuriarafat302@gmail.com>
2025-10-26 13:07:19 +0530

    Add Frontend URL to fix the CORS error

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "https://second-brain-alpha.vercel.app",
   "https://second-brain-backend-g2fo.onrender.com",
+  "https://www.secondbrain.services",
 ];
 app.use(express.json());
 app.use(
