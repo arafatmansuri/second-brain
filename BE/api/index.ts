2cce1de476f448a5516cdab87eafc79718399c94
arafatmansuri <mansuriarafat302@gmail.com>
2026-04-06 10:45:54 +0530

    Update gitignore

--- BE/api/index.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../src/index"; // path to compiled JS
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  return app(req, res); // forward request to Express
-};
